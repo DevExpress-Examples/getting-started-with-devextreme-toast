33741edeef61c1be58d6ee442d90ab3bdf343e45
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:22:28 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import notify from 'devextreme/ui/notify';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   types = ['error', 'info', 'success', 'warning'];
