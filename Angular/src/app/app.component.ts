7a43583b8b68981e57c442e7123948240df2232c
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:23:54 +0300

    Angular - Update to use standalone components

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import notify from 'devextreme/ui/notify';
 
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+
 @Component({
     selector: 'app-root',
+    imports: [DxButtonModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
 })
 export class AppComponent {
   types = ['error', 'info', 'success', 'warning'];
